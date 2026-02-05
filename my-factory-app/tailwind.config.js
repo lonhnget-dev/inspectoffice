42aedf5440bf94d2ea1bb06790c5f2fb427d5dbe
Your Name <lonhnget@gmail.com>
2026-02-05 17:57:05 +0700

    Added inspection dashboard and modal fixes

---
+++ my-factory-app/tailwind.config.js
@@ -0,0 +1,20 @@
+import flowbitePlugin from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        kantumruy: ['Kantumruy Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    flowbitePlugin,
+  ],
+}
